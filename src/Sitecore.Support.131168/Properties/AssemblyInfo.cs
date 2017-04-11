6ebc577ab0d6fb24eaf7b5a81a4d767da097fcf8
ido <ido@WS-IDO1UA1>
2017-04-11 16:50:35 +0300

    Added patch for XP 8.2.1 and 8.2.2

--- src/Sitecore.Support.131168/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.131168/Properties/AssemblyInfo.cs
@@ -4,3 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sitecore.Support.131168")]
 [assembly: AssemblyProduct("Sitecore.Support.131168")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Fixes issue with Parameters Template which have fields (e.g. Image field type) which prevent EE from save")]
+
